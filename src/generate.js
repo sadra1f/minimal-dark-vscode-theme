796e5910d9c88ec92ece2157d18d69847647cef4
Sadra Saderi <sadrassx@gmail.com>
2023-12-16 18:50:25 +0330

    Rename theme file

--- src/generate.js
+++ src/generate.js
@@ -4,12 +4,8 @@ const minimalDark = require("./minimal-dark");
 
 const DIST_DIR = "./dist";
 const THEMES_DIR = "./themes";
-const THEMES_POSTFIX = "-color-theme.json";
 
 fs.mkdirSync(DIST_DIR, { recursive: true });
 fs.mkdirSync(THEMES_DIR, { recursive: true });
 
-fs.writeFileSync(
-  `${THEMES_DIR}/Minimal Dark${THEMES_POSTFIX}`,
-  JSON.stringify(minimalDark.generate())
-);
+fs.writeFileSync(`${THEMES_DIR}/minimal-dark.json`, JSON.stringify(minimalDark.generate()));
