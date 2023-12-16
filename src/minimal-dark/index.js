75e751fc2d88c7e00ec356abb560ed7155c613df
Sadra Saderi <sadrassx@gmail.com>
2023-12-16 19:33:02 +0330

    Include dependencies in the generate function

--- src/minimal-dark/index.js
+++ src/minimal-dark/index.js
@@ -1,7 +1,36 @@
 const fs = require("fs");
 
-const theme = JSON.parse(fs.readFileSync("src/minimal-dark/minimal-dark.json", "utf-8"));
+const shared = require("../shared");
 
-exports.generate = function () {
-  return theme;
+const theme = shared.jsoncParse(fs.readFileSync("src/minimal-dark/minimal-dark.json", "utf-8"));
+
+exports.generate = () => {
+  const obj = {
+    ...shared.dependencies.dark_vs,
+    ...shared.dependencies.dark_plus,
+    ...theme,
+
+    colors: {
+      ...shared.dependencies.dark_vs.colors,
+      ...shared.dependencies.dark_plus.colors,
+      ...theme.colors,
+    },
+
+    tokenColors: [
+      ...shared.dependencies.dark_vs.tokenColors,
+      ...shared.dependencies.dark_plus.tokenColors,
+      ...theme.tokenColors,
+    ],
+
+    semanticTokenColors: {
+      ...shared.dependencies.dark_vs.semanticTokenColors,
+      ...shared.dependencies.dark_plus.semanticTokenColors,
+      ...theme.semanticTokenColors,
+    },
+  };
+
+  delete obj.include;
+  delete obj.$schema;
+
+  return obj;
 };
